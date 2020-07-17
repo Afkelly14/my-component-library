3eda10e8c4fcca129a8419eae8934a65b3f1b6b0
Alex_Kelly <afkelly14@Gmail.com>
2020-07-17 16:10:59 -0400

    all buttons added

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -8,26 +8,88 @@ function onClick() {
 }
 
 storiesOf("Button", module)
-  .add("Primary", () => (
-    <Button type="primary"  label="Primary" />
+  .add("Primary", () => <Button type="primary" label="Do Something" />)
+  .add("Danger", () => <Button type="danger" label="Do Something" />)
+  .add("Success", () => <Button type="success" label="Do Something" />)
+  .add("Warning", () => <Button type="warning" label="Do Something" />)
+  .add("Default", () => <Button type="default" label="Do Something" />)
+
+  .add("Primary Outline", () => (
+    <Button type="primary" outline label="Do Something" />
   ))
-  .add("Danger", () => <Button type="danger" label="Danger" />)
-  .add("Success", () => <Button type="success" label="Success" />)
-  .add("Warning", () => <Button type="warning" label="Warning" />)
-  .add("Default", () => <Button type="default" label="Default" />)
+  .add("Danger Outline", () => (
+    <Button type="danger" outline label="Do Something" />
+  ))
+  .add("Success Outline", () => (
+    <Button type="success" outline label="Do Something" />
+  ))
+  .add("Warning Outline", () => (
+    <Button type="warning" outline label="Do Something" />
+  ))
+  .add("Default Outline", () => (
+    <Button type="default" outline label="Do Something" />
+  ))
 
-  .add("Primary Outline", () => <Button type="primary" outline label="Primary" />)
-  .add("Danger Outline", () => <Button type="danger" outline label="Danger" />)
-  .add("Success Outline", () => <Button type="success" outline label="Success" />)
-  .add("Warning Outline", () => <Button type="warning" outline label="Warning" />)
-  .add("Default Outline", () => <Button type="default" outline label="Default" />)
+  .add("Primary Light", () => (
+    <Button type="primary" light label="Do Something" />
+  ))
+  .add("Danger Light", () => (
+    <Button type="danger" light label="Do Something" />
+  ))
+  .add("Success Light", () => (
+    <Button type="success" light label="Do Something" />
+  ))
+  .add("Warning Light", () => (
+    <Button type="warning" light label="Do Something" />
+  ))
+  .add("Default Light", () => (
+    <Button type="default" light label="Do Something" />
+  ))
 
-  .add("Primary Light", () => <Button type="primary" light label="Primary" />)
-  .add("Danger Light", () => <Button type="danger" light label="Danger" />)
-  .add("Success Light", () => <Button type="success" light label="Success" />)
-  .add("Warning Light", () => <Button type="warning" light label="Warning" />)
-  .add("Default Light", () => <Button type="default" light label="Default" />)
- 
+  .add("Large Primary", () => (
+    <Button type="primary" large label="Do Something" />
+  ))
+  .add("Large Danger", () => (
+    <Button type="danger" large label="Do Something" />
+  ))
+  .add("Large success", () => (
+    <Button type="success" large label="Do Something" />
+  ))
+  .add("Large Warning", () => (
+    <Button type="warning" large label="Do Something" />
+  ))
+  .add("Large Default", () => (
+    <Button type="default" large label="Do Something" />
+  ))
 
-  .add("Large Primary", () => <Button type="primary" large label="Large-Primary" />)
-  
+  .add("Large Light Primary", () => (
+    <Button type="primary" large light label="Do Something" />
+  ))
+  .add("Large Light Danger", () => (
+    <Button type="danger" large light label="Do Something" />
+  ))
+  .add("Large Light Success", () => (
+    <Button type="success" large light label="Do Something" />
+  ))
+  .add("Large Light Warning", () => (
+    <Button type="warning" large light label="Do Something" />
+  ))
+  .add("Large Light Default", () => (
+    <Button type="default" large light label="Do Something" />
+  ))
+
+  .add("Large Outline Primary", () => (
+    <Button type="primary" large outline label="Do Something" />
+  ))
+  .add("Large Outline Danger", () => (
+    <Button type="danger" large outline label="Do Something" />
+  ))
+  .add("Large Outline Success", () => (
+    <Button type="success" large outline label="Do Something" />
+  ))
+  .add("Large Outline Warning", () => (
+    <Button type="warning" large outline label="Do Something" />
+  ))
+  .add("Large Outline Default", () => (
+    <Button type="default" large outline label="Do Something" />
+  ));
